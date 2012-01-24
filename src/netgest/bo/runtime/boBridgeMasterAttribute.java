f2cf8d61e1b1c193c50dea798c4bd9e42f0fe400
Pedro Rio <pedro.rio@itds.pt>
2012-01-24 10:07:03 +0000

    Apagados comentários inuteis e com anos de existencia

--- src/netgest/bo/runtime/boBridgeMasterAttribute.java
+++ src/netgest/bo/runtime/boBridgeMasterAttribute.java
@@ -17,57 +17,6 @@ public class boBridgeMasterAttribute extends AttributeHandler {
         super(parent,def);
     }
  
-//    public void setValueString(String value, byte type) throws boRuntimeException {
-//        try
-//        {
-//            bridgeHandler bridge = (bridgeHandler)p_parent.getBridge(p_bridgename);
-//            ArrayList processedrows=new ArrayList();
-//            if(value!=null && value.trim().length()>0) {
-//                String[] sbouis = ClassUtils.splitToArray(value,";");
-//                for (int i = 0; i < sbouis.length; i++)  {
-//                    long cboui = Long.parseLong(sbouis[i]);
-//                    if(!bridge.haveBoui(cboui)) {
-//                        bridge.first();
-//                        bridge.add(cboui);
-//                    }
-//                    Integer xrow = new Integer(bridge.getRslt().getRow());
-//                    if(processedrows.indexOf(xrow)==-1)
-//                        processedrows.add(xrow);
-//                }
-//            }
-//            
-//            if(processedrows.size()!=bridge.getRowCount()) 
-//            {
-//                bridge.beforeFirst();
-//                while(bridge.next()) 
-//                {
-//                    Integer xrow = new Integer(bridge.getRslt().getRow());
-//                    if(processedrows.indexOf(xrow)==-1) 
-//                    {
-//                        boolean removed = bridge.remove();
-//                        if( removed )
-//                        {
-//                            bridge.previous();
-//                        } 
-//                        processedrows.remove(xrow);
-//                        if( removed )
-//                        {
-//                            for (int i = 0; i < processedrows.size(); i++) 
-//                            { 
-//                                if(((Integer)processedrows.get(i)).intValue() > bridge.getRslt().getRow())
-//                                    processedrows.set(i,new Integer(((Integer)processedrows.get(i)).intValue()-1)); 
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        catch (SQLException e)
-//        {
-//            throw new boRuntimeException2(this.getClass().getName()+"setValueString() error.\n"+e.getClass().getName()+"\n"+e.getMessage());
-//        }
-//        setInputType(type);
-//    }
     /**
      * Coloca a bridge com os bouis que vem no value.Faz o ADD e o REMOVE se necessário.
      * No fim tenta deixar a BRIDGE na mesma posição relativa com que entrou.Se não conseguir
@@ -86,7 +35,6 @@ public class boBridgeMasterAttribute extends AttributeHandler {
             bridgeHandler bridge = (bridgeHandler)getParent().getBridge(getName());
             int savePos = bridge.getRow();
             ArrayList processedrows=new ArrayList();
-            boBridgeRow row = null;
             if(value!=null && value.trim().length()>0) {
                 String[] sbouis = value.split(";"); // ClassUtils.splitToArray(value,";");
                 
@@ -210,40 +158,40 @@ public class boBridgeMasterAttribute extends AttributeHandler {
     
 //override
     public  boolean validate() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).validate();
     }
     public  boolean required() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).required();
     }
     public  boolean disableWhen() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).disableWhen();
     }
     public  boolean hiddenWhen() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).hiddenWhen();
     }
     public  boolean haveDefaultValue(){
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return false;
     }
     public  String defaultValue() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).defaultValue();
     }
     public  String[] condition() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).condition();
     }
     public  boolean canChangeLov() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).canChangeLov();
     }  
     
     public  String formula() throws boRuntimeException {
-        // TODO:  Overrided on generated objects
+        // Overridden on generated objects
         return ((bridgeHandler)getParent().getBridge(getName())).formula();
     } 
 }
