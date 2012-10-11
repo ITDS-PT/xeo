f1ec834306e6969dd05002ac70dfde152f9b13fa
Antonio Cruz <antonio.cruz@itds.pt>
2012-10-11 12:51:14 +0000

    Alteração que permite o autologin no XEO. Por omissão se existir um parametro da JVM xeo.autologin=true faz login com SYSUSER se existir o parametro xeo.autologinuser=<USER> faz login com esse utilizador (o outro parametro não necessita estar presente). Existe ainda um 3ªparametro que permite atribuir um profile automaticamente a um user xeo.autologinprofile=<nomedoperfil>

--- src/netgest/bo/http/XEOBuilderFilter.java
+++ src/netgest/bo/http/XEOBuilderFilter.java
@@ -10,22 +10,32 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import netgest.bo.builder.boBuilder;
 import netgest.bo.builder.boBuilderOptions;
 import netgest.bo.builder.boBuilderProgress;
 import netgest.bo.localizations.MessageLocalizer;
 import netgest.bo.runtime.EboContext;
+import netgest.bo.runtime.boObject;
+import netgest.bo.runtime.boObjectList;
+import netgest.bo.runtime.boRuntimeException;
 import netgest.bo.system.boApplication;
 import netgest.bo.system.boLoginBean;
 import netgest.bo.system.boLoginException;
 import netgest.bo.system.boSession;
+import netgest.bo.utils.IProfileUtils;
 
 public class XEOBuilderFilter implements Filter {
 
 	public static boolean developmentMode=false;
 	
 	public static boolean running=false;
+	private boolean autologin=false;
+	public String autologinUser="SYSUSER";
+	public String autologinprofile="";
+	
+	
 	boolean buildSucess=true;
 	HttpServletRequest request;
 	HttpServletResponse response;
@@ -35,11 +45,43 @@ public class XEOBuilderFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {		
+			FilterChain chain) throws IOException, ServletException {
+		this.request = (HttpServletRequest)request;
+		this.response = (HttpServletResponse)response;
+		boSession session = this.getBoSession();
+	
+		if (autologin && session==null)
+		{
+			 EboContext ctx = null;
+			 boApplication bapp = boApplication
+						.getApplicationFromStaticContext("XEO");
+					 bapp.suspendAgents();
+					try {
+						session = bapp.boLogin(autologinUser, boLoginBean
+							.getSystemKey());
+						this.getHttpSession(true).setAttribute("boSession", session);
+						
+						if (this.autologinprofile!=null && !this.autologinprofile.equals(""))
+						{
+							ctx=session.createRequestContext(null, null, null);
+							
+							boObject profile=IProfileUtils.getProfileByName(ctx, this.autologinprofile);
+							if (profile.exists())
+								session.setPerformerIProfileBoui(Long.toString(profile.getBoui()));
+						}
+					} catch (boLoginException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (boRuntimeException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	
+					finally {
+						if (ctx!=null) ctx.close();
+					}
+		}
 		if (developmentMode)
 		{
-			this.request = (HttpServletRequest)request;
-			this.response = (HttpServletResponse)response;
 			long builderLastRun=boBuilder.getXEOStudioBuilderLastRun();
 			if (builderLastRun>0 && !running)
 			{
@@ -61,10 +103,19 @@ public class XEOBuilderFilter implements Filter {
 	public void init(FilterConfig config) throws ServletException {
 		
 		String xeodev=System.getProperty("xeo.development");
+		String alogin=System.getProperty("xeo.autologin");
+		this.autologinUser=System.getProperty("xeo.autologinuser")!=null?System.getProperty("xeo.autologinuser"):
+				this.autologinUser;
+		this.autologinprofile=System.getProperty("xeo.autologinprofile");
+		
 		if (xeodev!=null && (xeodev.equalsIgnoreCase("true") ||
 				xeodev.equalsIgnoreCase("yes"))){
 			developmentMode=true;
-			boApplication.getDefaultApplication().setDevelopmentMode(true);
+			boApplication.getDefaultApplication().setDevelopmentMode(true);								
+		}
+		if ((alogin!=null && (alogin.equalsIgnoreCase("true") ||
+				alogin.equalsIgnoreCase("yes"))) || this.autologinUser!=null){
+			this.autologin = true;							
 		}
 
 	}
@@ -122,5 +173,21 @@ public class XEOBuilderFilter implements Filter {
 				 request.getRequestURL().toString().indexOf("login.jsp")>-1) 
 			 toRet=true;
 		 return toRet;
+	}
+	
+	private HttpSession getHttpSession(boolean createNew) {
+		HttpServletRequest req = (HttpServletRequest) this.request;
+		HttpSession session = req.getSession(createNew);
+		return session;
+	}
+	
+	private boSession getBoSession() {
+		HttpSession session = getHttpSession(false);
+		if (session != null) {
+			boSession xeoSession = (boSession) session
+					.getAttribute("boSession");
+			return xeoSession;
+		}
+		return null;
 	}
 }
