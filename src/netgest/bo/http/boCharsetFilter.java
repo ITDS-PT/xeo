99daf8ea8488f124fc39a46cf0bebff2877dda65
João Carreira <jptcarreira@gmail.com>
2017-08-29 16:11:37 +0100

    Clean code

--- src/netgest/bo/http/boCharsetFilter.java
+++ src/netgest/bo/http/boCharsetFilter.java
@@ -22,42 +22,13 @@ public class boCharsetFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next)
         throws IOException, ServletException {
         
-        /*
-        String path = ((HttpServletRequest)request).getRequestURI();
-        
-        Matcher m1 = p1.matcher( path );
-        Matcher m2 = p2.matcher( path );
-        
-        if( m1.matches() && request.getAttribute("__forwardResourceGIF") == null ) { 
-        	String newUril = "/.xeodeploy/resources/"+m1.group(3)+".gif";
-            RequestDispatcher dispatch = request.getRequestDispatcher( newUril );
-            if( dispatch != null ) {
-            	request.setAttribute("__forwardResourceGIF",Boolean.TRUE);
-            	dispatch.forward( request, response );
-            }
-            else {
-            	next.doFilter(request, response);            	
-            }
-		}
-        else if( m2.matches() && request.getAttribute("__forwardResourceGIF") == null ) { 
-        	String newUril = "/.xeodeploy/resources/"+m2.group(2)+".gif";
-            RequestDispatcher dispatch = request.getRequestDispatcher( newUril );
-            if( dispatch != null ) {
-            	request.setAttribute("__forwardResourceGIF",Boolean.TRUE);
-            	dispatch.forward( request, response );
-            }
-            else {
-            	next.doFilter(request, response);            	
-            }
-		}
-        else {
-        */
-            // Respect the client-specified character encoding
-            // (see HTTP specification section 3.4.1)
-            if(null == request.getCharacterEncoding())
-                request.setCharacterEncoding(encoding);
-        	next.doFilter(request, response);
-        //}
+        // Respect the client-specified character encoding
+        // (see HTTP specification section 3.4.1)
+        if(null == request.getCharacterEncoding())
+            request.setCharacterEncoding(encoding);
+            
+    	next.doFilter(request, response);
+    	
         
     }
 
